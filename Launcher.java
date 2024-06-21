d29bcff4a5f71834897f9670fbd0dfd7b298e702
leoleaipailote <136939887+leoleaipailote@users.noreply.github.com>
2024-06-21 03:12:48 -0400

    hello

--- Launcher.java
+++ Launcher.java
@@ -1,11 +1,11 @@
 //Contains main... run me to launch the game!
-public class Launcher{
-           
-    //Initializes and launches the game
-    public static void main(String[] args){              
+public class Launcher {
+
+    // Initializes and launches the game
+    public static void main(String[] args) {
         AbstractGame game = new LeaipailoteGame();
-        game.setDebugMode(true);
-        game.play();    
-    }        
-    
+        game.setDebugMode(false);
+        game.play();
+    }
+
 }
